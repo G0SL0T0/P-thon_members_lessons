974ea888c7928f08c6895e97bcdb574271968193
Gosloto <182990710+G0SL0T0@users.noreply.github.com>
2024-09-30 18:29:29 +0300

    Create Lesson_1 _IF_.py

---
+++ Lesson_1 _IF_.py
@@ -0,0 +1,28 @@
+import random
+
+def int_random(number_random):
+    return random.randint(0,11)
+
+number_random =0
+exit_while_1=0
+while exit_while_1 == 0:
+    number_random =int_random(number_random)
+    if number_random == 5:
+        print("Победа")
+        exit_while_1+=1
+    elif number_random == 6:
+        print("Цифра 6")
+        exit_while_1+=1
+    elif number_random == 7:
+        print("Цифра 7")
+        exit_while_1+=1
+    elif number_random == 8:
+        print("Цифра 8")
+        exit_while_1+=1
+    else:
+        print("Проиграл")
+        user_input = input("Продолжить? 1 - да, 2 - закончить.")
+        if user_input == 1:
+            exit_while_1=0
+        elif user_input == 2:
+            exit_while_1=1
